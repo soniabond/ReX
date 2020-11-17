571ba2f83ffcc9d10e92f45b94db6a226ed1e8c8
Sonia Bondarenko <soniabondarenko1@gmail.com>
2020-11-17 20:23:08 +0200

    new

--- venv/app/ReX laba1/ReX2LTS.py
+++ venv/app/ReX laba1/ReX2LTS.py
@@ -25,28 +25,42 @@ class ReX2LTS:
         thirdTransEnd = None
         for token in strTree:
             if token.isalpha():
-                if secondTrans == None:
+                if firstTransStart == None:
                     transitions.insert(counter, [startState, token, lastState]);
                     firstTransStart = transitions[counter]
                     firstTransEnd = transitions[counter]
                     counter += 1
-                else:
+                elif secondTrans == None:
                     transitions.insert(counter, [startState, token, lastState]);
                     secondTransStart = transitions[counter]
                     secondTransEnd = transitions[counter]
+                    counter += 1
+                else:
+                    transitions.insert(counter, [startState, token, lastState]);
+                    thirdTransStart = transitions[counter]
+                    thirdTransEnd = transitions[counter]
                     counter += 1
             if token == ',':
                 newEndFirst = "s"+str(self.states.__len__())
                 self.states.append(newEndFirst)
                 newStartSecond = "s" + str(self.states.__len__())
                 self.states.append(newStartSecond)
-                firstTransEnd[2]=newEndFirst
-                secondTransStart[0]=newStartSecond
-                transitions.insert(counter, [newEndFirst, emptyWord, newStartSecond]);
-                counter+=1
-                firstTransEnd = secondTransEnd
-                secondTransEnd = None
-                secondTransStart = None
+                if thirdTransStart == None:
+                    firstTransEnd[2]=newEndFirst
+                    secondTransStart[0]=newStartSecond
+                    transitions.insert(counter, [newEndFirst, emptyWord, newStartSecond]);
+                    counter+=1
+                    firstTransEnd = secondTransEnd
+                    secondTransEnd = None
+                    secondTransStart = None
+                else:
+                    secondTransEnd[2] = newEndFirst
+                    thirdTransStart[0] = newStartSecond
+                    transitions.insert(counter, [newEndFirst, emptyWord, newStartSecond]);
+                    counter += 1
+                    secondTransEnd = thirdTransEnd
+                    thirdTransEnd = None
+                    thirdTransStart = None
 
             if token == '|':
                 newStartFirst = "s" + str(self.states.__len__())
@@ -57,16 +71,77 @@ class ReX2LTS:
                 self.states.append(newStartSecond)
                 newEndSecond = "s" + str(self.states.__len__())
                 self.states.append(newEndSecond)
-                transitions.insert(counter, [startState, emptyWord, newStartFirst]);
-                firstTransStart[0] = newStartFirst
+                if thirdTransStart == None:
+                    transitions.insert(counter, [startState, emptyWord, newStartFirst]);
+                    firstTransStart[0] = newStartFirst
+                    firstTransStart = transitions[counter]
+                    counter += 1
+                    transitions.insert(counter, [startState, emptyWord, newStartSecond]);
+                    secondTransStart[0] = newStartSecond
+                    secondTransStart = None
+                    counter += 1
+                    transitions.insert(counter, [newEndFirst, emptyWord, lastState]);
+                    firstTransEnd[2] = newEndFirst
+                    firstTransEnd = transitions[counter]
+                    counter += 1
+                    transitions.insert(counter, [newEndSecond, emptyWord, lastState]);
+                    secondTransEnd[2] = newEndSecond
+                    secondTransEnd = None
+                    counter += 1
+                else:
+                    transitions.insert(counter, [startState, emptyWord, newStartFirst]);
+                    secondTransStart[0] = newStartFirst
+                    secondTransStart = transitions[counter]
+                    counter += 1
+                    transitions.insert(counter, [startState, emptyWord, newStartSecond]);
+                    thirdTransStart[0] = newStartSecond
+                    thirdTransStart = None
+                    counter += 1
+                    transitions.insert(counter, [newEndFirst, emptyWord, lastState]);
+                    secondTransEnd[2] = newEndFirst
+                    secondTransEnd = transitions[counter]
+                    counter += 1
+                    transitions.insert(counter, [newEndSecond, emptyWord, lastState]);
+                    thirdTransEnd[2] = newEndSecond
+                    thirdTransEnd = None
+                    counter += 1
+
+            if token == '*':
+                newStart = "s" + str(self.states.__len__())
+                self.states.append(newStart)
+                newEnd = "s" + str(self.states.__len__())
+                self.states.append(newEnd)
+                transitions.insert(counter, [newStart, emptyWord, newEnd]);
                 counter+=1
-                firstTransStart = transitions[counter-1]
-                transitions.insert(counter, [startState, emptyWord, newStartSecond]);
-                secondTransStart = None
-                counter += 1
-                transitions.insert(counter, [startState, emptyWord, newStartFirst]);
-                firstTransStart[0] = newStartFirst
-                counter += 1
+                transitions.insert(counter, [newEnd, emptyWord, newStart]);
+                counter+=1
+                if thirdTransStart != None:
+                    transitions.insert(counter, [startState, emptyWord, newStart]);
+                    thirdTransStart[0]=newStart
+                    thirdTransStart = transitions[counter]
+                    counter+=1
+                    transitions.insert(counter, [newEnd, emptyWord, lastState]);
+                    thirdTransEnd[2] = newEnd
+                    thirdTransEnd = transitions[counter]
+                    counter += 1
+                elif secondTransStart != None:
+                    transitions.insert(counter, [startState, emptyWord, newStart]);
+                    secondTransStart[0]=newStart
+                    secondTransStart = transitions[counter]
+                    counter+=1
+                    transitions.insert(counter, [newEnd, emptyWord, lastState]);
+                    secondTransEnd[2] = newEnd
+                    secondTransEnd = transitions[counter]
+                    counter += 1
+                else:
+                    transitions.insert(counter, [startState, emptyWord, newStart]);
+                    firstTransStart[0]=newStart
+                    firstTransStart = transitions[counter]
+                    counter+=1
+                    transitions.insert(counter, [newEnd, emptyWord, lastState]);
+                    firstTransEnd[2] = newEnd
+                    firstTransEnd = transitions[counter]
+                    counter += 1
 
 
 
